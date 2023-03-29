b6305e12c48da07048477f810c8dd6370a94903e
joaoalberis <alberisjoao@hotmail.com>
2023-03-29 11:27:38 -0300

    alterações no controller

--- src/main/java/tech/joaoalberis/controller/UsuarioController.java
+++ src/main/java/tech/joaoalberis/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import tech.joaoalberis.service.UsuarioService;
 
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
 
     @Autowired
